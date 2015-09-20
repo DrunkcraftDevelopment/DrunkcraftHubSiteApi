1e67805519a66365c54cc9956f92b64a581889b3
jdollar <dollarhidejoel@gmail.com>
2015-09-20 11:31:49 -0400

    Initial commit. Has some database functionality and api navigation

---
+++ databaseScripts/create_news_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE news (
+    news_id int(11) NOT NULL AUTO_INCREMENT,
+    news_title varchar(255) NOT NULL,
+    news_text varchar(255) NOT NULL,
+    news_author varchar(255) NOT NULL,
+    PRIMARY KEY(news_id)
+); 
