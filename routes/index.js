c623a7190048285b017855bd54ce4399eaf4126e
jdollar <dollarhidejoel@gmail.com>
2015-09-21 20:40:39 -0400

    Allow for same origin requests

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,17 @@ var app = require('../server')
 var models = require('../models')
 
 var router = express.Router()
-
 router.get('/', function(req, res) {
     res.json({message:'The api server is running!'})
 })
 
 app.use('/api', router)
+
+//This is required to allow for same origin navigation
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 module.exports = router
